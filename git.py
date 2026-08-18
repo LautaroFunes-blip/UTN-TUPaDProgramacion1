cb071d53740488c6cc7865a74e17cb2ca7763510
Lautaro <joaquinlautarofunes62662@gmail.com>
2026-08-18 12:55:36 -0300

    prueba de actualizacion

--- git.py
+++ git.py
@@ -1,1 +1,6 @@
-print('git funciona correctamente')
+lista_nombres=['Uriel', 'Lautaro']
+nombre=input('ingrese el nomrbe de usuario').capitalize
+if nombre in lista_nombres:
+    print('funciono')
+else:
+    print('error fatal')
